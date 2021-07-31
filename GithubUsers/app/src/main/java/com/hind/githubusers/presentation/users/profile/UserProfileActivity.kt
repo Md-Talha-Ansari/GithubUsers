9037677595c746eb76a220bb9548f8e70e7f4da1
Md-Talha-Ansari <49421059+Md-Talha-Ansari@users.noreply.github.com>
2021-07-31 12:34:24 +0900

    Added user profile activity.
    User profile activity added.

---
+++ GithubUsers/app/src/main/java/com/hind/githubusers/presentation/users/profile/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.hind.githubusers.presentation.users.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hind.githubusers.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
